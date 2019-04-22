919c58ab70b76f2bc7cd637f788ee9aaf5655b36
Chase Gruber <cgruber@esri.com>
2019-04-22 10:03:22 -0400

    tests working

---
+++ app/initializers/form-ui.js
@@ -0,0 +1,11 @@
+import TextField from '@ember/component/text-field';
+import Checkbox from '@ember/component/checkbox';
+
+export function initialize () {
+  TextField.reopen({
+    attributeBindings: ['aria-label', 'aria-describedby', 'aria-labelledby']
+  });
+  Checkbox.reopen({
+    attributeBindings: ['aria-label', 'aria-describedby', 'aria-labelledby']
+  });
+}
