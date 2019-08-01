ff1b67739e1aa3e9111b50fb172a5ea837d8f089
Chase Gruber <cgruber@esri.com>
2019-08-01 13:37:23 -0400

    textarea initializer

--- app/initializers/form-ui.js
+++ app/initializers/form-ui.js
@@ -1,8 +1,12 @@
 import TextField from '@ember/component/text-field';
+import TextArea from '@ember/component/text-area';
 import Checkbox from '@ember/component/checkbox';
 
 export function initialize () {
   TextField.reopen({
+    attributeBindings: ['aria-label', 'aria-describedby', 'aria-labelledby']
+  });
+  TextArea.reopen({
     attributeBindings: ['aria-label', 'aria-describedby', 'aria-labelledby']
   });
   Checkbox.reopen({
